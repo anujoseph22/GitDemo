481ae567ec81cdf20087c5a16a8d6cb0de5e4abf
anu <anna.anujoseph@gmail.com>
2019-06-24 13:56:20 +0100

    first commit

---
+++ Portfolio/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movies = new Movie() { name = "Interstellar" };
+            return View(movies);
+        }
+    }
+}
